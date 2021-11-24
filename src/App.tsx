f858fa293c6165b6c220aeb8c11bfac44ccc378d
SeobisBack <seobisback@gmail.com>
2021-11-24 12:58:36 +0900

    Use Typescript, React js make Coin view

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Router from './Router';
+
+
+function App() {
+  return (
+    <Router />
+  );
+}
+
+export default App;

17643fda84defc133e91e9a9c53494da020861f2
SeobisBack <seobisback@gmail.com>
2021-11-25 01:54:04 +0900

    Use React Query

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding : 0px 20px; 
@@ -52,7 +54,7 @@ const Img = styled.img`
     margin-right : 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
     id: string,
     name: string,
     symbol: string,
@@ -63,24 +65,25 @@ interface CoinInterface {
 }
 
 function Coins() {
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        (async() => {
-           const response = await fetch("https://api.coinpaprika.com/v1/coins");
-           const json = await response.json();
-           setCoins(json.slice(0, 100));
-           setLoading(false);
-        })();
-    }, []);
-    console.log(coins);
+    // const [coins, setCoins] = useState<ICoin[]>([]);
+    // const [loading, setLoading] = useState(true);
+    // useEffect(() => {
+    //     (async() => {
+    //        const response = await fetch("https://api.coinpaprika.com/v1/coins");
+    //        const json = await response.json();
+    //        setCoins(json.slice(0, 100));
+    //        setLoading(false);
+    //     })();
+    // }, []);
+    // console.log(coins);
+    const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins)
     return (
         <Container>
             <Header>
-                <Title>Coins</Title>
+                <Title>SeobisCoins</Title>
             </Header>
-            {loading ? <Loader>Now Loading ... </Loader> : <CoinsList>
-                {coins.map(coin => <Coin key={coin.id}>
+            {isLoading ? <Loader>Now Loading ... </Loader> : <CoinsList>
+                {data?.slice(0, 100).map((coin) => <Coin key={coin.id}>
                     <Link to={{
                         pathname: `/${coin.id}`,
                         state: { name:coin.name },

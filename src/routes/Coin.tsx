04121e0ba6575c3044459b7cae3f177e6a8d0bbc
SeobisBack <seobisback@gmail.com>
2021-11-24 17:52:58 +0900

    Make Detail Page

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -15,6 +15,31 @@ const Header = styled.header`
     align-items: center;
 `;
 
+const Overview = styled.div`
+    display : flex;
+    justify-content: space-between;
+    background-color : rgba(0,0,0,0.5);
+    padding : 10px 20px;
+    border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    span: first-child {
+        font-size : 10px;
+        font-weight : 400;
+        text-transform : uppercase;
+        margin-bottom : 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin : 20px 0px;
+`;
+
 const Title = styled.h1`
     font-size : 48px;
     color : ${props => props.theme.accentColor};
@@ -109,14 +134,45 @@ function Coin() {
             console.log(priceData);
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, [])
+    }, [coinId])
     return (
         <Container>
             <Header>
-                <Title>{`✔︎ - ${state?.name}` || 'Loading...'}</Title>
+                <Title>{`✔︎ - ${state?.name}` ? state.name : loading ? 'Loading...' : info?.name}</Title>
             </Header>
-            {loading ? <Loader>Now Loading ... </Loader> : null}
+            {loading ? <Loader>Now Loading ... </Loader> : (
+                <>
+                <Description>First Create Date : {priceInfo?.first_data_at}</Description>
+                <Overview>
+                    <OverviewItem>
+                        <span>Rank</span>
+                        <span>{info?.rank}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Symbol</span>
+                        <span>{info?.symbol}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Open Source</span>
+                        <span>{info?.open_source ? "Yes" : "No"}</span> 
+                    </OverviewItem>
+                </Overview>
+                <Description>{info?.description}</Description>
+                <Overview>
+                    <OverviewItem>
+                        <span>Total Supply</span>
+                        <span>{priceInfo?.total_supply}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Max Supply</span>
+                        <span>{priceInfo?.max_supply}</span>
+                    </OverviewItem>
+                </Overview>
+                <Description>Last Update : {priceInfo?.last_updated}</Description>
+                </>
+            )}
         </Container>
     );
 }
